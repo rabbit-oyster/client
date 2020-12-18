4547c2eb42cb7c4210211b650f1ed315a8d7d496
원더 <wonderbotsupport@naver.com>
2020-12-19 04:36:46 +0900

    랜딩페이지 업데이트
    Co-Authored-By: Sangoon_Is_Noob <40298015+sannoob@users.noreply.github.com>

--- src/routes/Main.jsx
+++ src/routes/Main.jsx
@@ -2,52 +2,51 @@ import React, { useState, useEffect } from 'react'
 import { Link } from 'react-router-dom'
 import { Container, Header, Icon, Button, Grid } from 'semantic-ui-react'
 import styles from 'styled-components'
-import Pageable from 'pageable'
+
 import { motion } from 'framer-motion'
-import { disableBodyScroll } from 'body-scroll-lock'
 import Message from '../components/Message'
 import MessageList from '../components/MessageList'
 
 const Section = styles.section`
-  height: 100vh;
+  minHeight: 100vh;
 `
 export default function Main () {
-  const [page, setPage] = useState(0)
-  const [pages, setPages] = useState(null)
-  useEffect(() => {
-    const p = new Pageable('#main', {
-      animation: 500,
-      autoInitialize: false
-    })
-    setPages(p)
-    disableBodyScroll(document.querySelector('#root'))
-    p.on('init', e => {
-      setPage(e.index)
-    })
-    p.on('scroll.start', e => {
-      setPage(e.index)
-    })
-  }, [])
   return (
     <>
-      <Container textAlign='center' style={{ height: '100vh', width: '100%', background: '#fefefe' }} id='main'>
-        <Section id='hero' style={{ textAlign: 'center', paddingTop: '30vh' }} currentPage={page} data-anchor='hero'>
-          <Header as='h1' style={{ fontSize: '7vh' }}>마음의 숲</Header>
-          <h2><Icon name='quote left' />자신도 몰랐던 자신의 마음 속을 들여다보세요.<Icon name='quote right' /></h2>
-          <div style={{ minHeight: '2vh' }} />
-          <Button size='large' href='#about' circular>
-            더 알아보기
-          </Button>
+      <div style={{ paddingBottom: '100px' }}>
+        <Section id='hero' style={{ textAlign: 'center', padding: '25vh 0' }}>
+          <Container textAlign='center'>
+            <motion.div
+              initial={{ scale: 0 }}
+              animate={{ scale: 1 }}
+              transition={{
+                duration: 0.5,
+                type: 'spring',
+                stiffness: 260,
+                damping: 20
+              }}
+            > <h1><Icon name='tree' color='green' /></h1>
+              <Header as='h1' style={{ fontSize: '7vh' }}>마음의 숲</Header>
+              <br />
+              <Icon name='quote left' />
+              <h2>자신도 몰랐던 자신의 마음 속을 들여다보세요.</h2>
+              <Icon name='quote right' />
+              <div style={{ minHeight: '2vh' }} />
+              <Button size='large' href='#about' circular>
+                더 알아보기
+              </Button>
+            </motion.div>
+
+          </Container>
         </Section>
         <Section
-          id='about' style={{ background: '#f6f6f6' }} currentPage={page} destroyPageable={() => {
-            if (pages) {
-              pages.destroy()
-            }
-          }}
-          data-anchor='about'
+          style={{ background: '#f6f6f6' }}
         >
-          <Container textAlign='center'>
+          <svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 1440 320'>
+            <path fill='#ffffff' fillOpacity='1' d='M0,256L48,218.7C96,181,192,107,288,80C384,53,480,75,576,117.3C672,160,768,224,864,240C960,256,1056,224,1152,192C1248,160,1344,128,1392,112L1440,96L1440,0L1392,0C1344,0,1248,0,1152,0C1056,0,960,0,864,0C768,0,672,0,576,0C480,0,384,0,288,0C192,0,96,0,48,0L0,0Z' />
+          </svg>
+          <Container textAlign='center' style={{ paddingTop: '50px' }}>
+            <section id='about' />
             <Grid stackable>
               <Grid.Column mobile={16} computer={8}>
                 <div className='device iphone-x' style={{ transform: 'scale(0.5)' }} textAlign='center'>
@@ -76,24 +75,32 @@ export default function Main () {
                   </div>
                 </div>
               </Grid.Column>
-              <Grid.Column mobile={16} computer={8} textAlign='center' style={{ padding: '25vh 0' }}>
+              <Grid.Column mobile={16} computer={8} textAlign='center' style={{ padding: '20vh 0' }}>
                 <Header as='h2' style={{ fontSize: '4vh' }}>고민이 있으세요?</Header>
                 <h3>AI를 통한 우울증 진단으로 마음의 병을 <br />미리 진단하고, 자살을 예방합니다.</h3><br />
-                <Button icon labelPosition='right' color='blue' size='large' as={Link} to='/chatBot' circular>
+                <Button icon labelPosition='right' color='blue' size='large' href='/chatBot' circular>
                   검사받기
                   <Icon name='arrow right' />
                 </Button>
               </Grid.Column>
             </Grid>
           </Container>
+
         </Section>
-        <Section id='github' style={{ textAlign: 'center', padding: '25vh 0' }} currentPage={page} data-anchor='github'>
-          <Header as='h2' style={{ fontSize: '4vh' }}>오픈소스</Header>
+        <svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 1440 320'>
+          <path fill='#f6f6f6' fillOpacity='1' d='M0,256L48,218.7C96,181,192,107,288,80C384,53,480,75,576,117.3C672,160,768,224,864,240C960,256,1056,224,1152,192C1248,160,1344,128,1392,112L1440,96L1440,0L1392,0C1344,0,1248,0,1152,0C1056,0,960,0,864,0C768,0,672,0,576,0C480,0,384,0,288,0C192,0,96,0,48,0L0,0Z' />
+        </svg>
+        <Section id='github' style={{ textAlign: 'center' }}>
+          <Header as='h2' style={{ fontSize: '4vh', padding: '10vh 0' }}>오픈소스</Header>
           <h3>모든 소스코드는 Github에 공개되어있습니다.</h3>
-          <Button className='github'>프론트앤드</Button>
-          <Button className='github'>백앤드</Button>
+          <h4>Made with ❤️</h4>
+          <p>
+            <h5>사용한 데이터셋</h5>
+            <p>전국건강증진센터표준데이터.csv<br />정신건강관련전체기관정보_2019.csv<br />KOSAC Sentiment Polarity Lexicon</p>
+          </p>
+          <Button secondary href='https://github.com/rabbit-oyster/The-forest-of-the-mind'><Icon name='github' /> Github에서 보기</Button>
         </Section>
-      </Container>
+      </div>
     </>
   )
 }
