dc3f77b478051097a7845bb45b097811e5bd52c7
원더 <wonderbotsupport@naver.com>
2020-12-18 10:46:47 +0900

    index.jsx 파일 되돌리기

--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import App from './src/App.jsx'
+import App from './App.jsx'
 import 'bootstrap/dist/css/bootstrap.min.css'
 
 ReactDOM.render(
