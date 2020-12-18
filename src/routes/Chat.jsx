89b05dfda3a9b9587f0b162bb77a4ad476d1ef16
원더 <wonderbotsupport@naver.com>
2020-12-19 08:15:39 +0900

    채팅, 챗봇 수정
    Co-Authored-By: Sangoon_Is_Noob <40298015+sannoob@users.noreply.github.com>

--- src/routes/Chat.jsx
+++ src/routes/Chat.jsx
@@ -1,29 +1,64 @@
 /* eslint-disable no-unused-vars */
-import React, { useState } from 'react'
-import Chat, { Bubble, useMessages } from '@chatui/core'
+import React, { useEffect, useState } from 'react'
+import Chat, { Bubble, useMessages, toast } from '@chatui/core'
 import styled from 'styled-components'
 // eslint-disable-next-line import/no-webpack-loader-syntax
 import ChineseCSS from '!!raw-loader!@chatui/core/dist/index.css'
+import { baseURL } from '../utils/REST'
+import io from 'socket.io-client'
+import Logger from '../utils/Logger'
+import Loading from '../components/Loading'
 
 // Your code stuff...
 
-function Chats () {
-  function handleSend (type, val) {
-    if (type === 'text' && val.trim()) {
+export default function Chats () {
+  const [state, setState] = useState(0) // 0: Loading | 1: Ready | 2: Errored | 3: sending request | 4: Start
+  const [reconnecting, setReconnection] = useState(false)
+  const [data, setData] = useState(null)
+  const [socket, setSocket] = useState(null)
+
+  useEffect(() => {
+    const socketConn = io(baseURL + '/chat')
+    socketConn.on('connect', () => {
+      Logger.info(`[Socket.IO] [connect] Socket Connected ${baseURL}`)
+      setSocket(socketConn)
+      setState(1)
+      setReconnection(false)
+      socketConn.emit('setRoom', { roomId: '6974' })
+      toast.success('서버와 연결되었습니다 :>')
+    })
+    socketConn.on('userJoined', (data) => {
+      console.log(data)
+      Logger.debug('[Socket.IO] [userJoined] the User have joined.')
+      toast.success('유저가 입장했습니다.')
+      setState(4)
+    })
+    socketConn.on('messageDenied', (data) => {
+      Logger.debug(`[Socket.IO] [messageDenied] messageDenied ${data.content}`)
+      toast.fail('메세지 내용에 비속어가 담겨있어 전송이 취소되었어요.')
+    })
+    socketConn.on('messageAccepted', (data) => {
+      Logger.debug(`[Socket.IO] [messageAccepted] appendMsg(${data.content})`)
       appendMsg({
         type: 'text',
-        content: { text: val },
+        content: { text: data.content },
         position: 'right'
       })
-
-      setTyping(true)
-
-      setTimeout(() => {
-        appendMsg({
-          type: 'text',
-          content: { text: 'Bala bala' }
-        })
-      }, 1000)
+    })
+    socketConn.on('disconnect', () => {
+      Logger.warn(`[Socket.IO] [disconnect] Socket Disconnected ${baseURL}`)
+      setReconnection(true)
+    })
+  }, [reconnecting])
+  function handleSend (type, content) {
+    content = content.trim()
+    if (content.length <= 0) return
+    if (content.length <= 4) return toast.fail('보낼 메세지의 길이는 4글자 이상이여야 해요.')
+    if (state === 3) return
+    Logger.debug(`[Chat] Message Typed ${content} Type: ${type}`)
+    if (type !== 'text') return Logger.warn('[Chat] UnSupported Message Type')
+    else {
+      socket.emit('sendMessage', { content })
     }
   }
 
@@ -32,16 +67,25 @@ function Chats () {
     return <Bubble content={content.text} style={{ backgroundColor: position === 'right' ? '#0084ff' : '#eee' }} />
   }
   const { messages, appendMsg, setTyping } = useMessages([])
-  console.log(ChineseCSS)
   return (
     <>
-      <Chat
-        placeholder='메세지를 입력해주세요.'
-        navbar={{ title: '상담' }}
-        messages={messages}
-        renderMessageContent={renderMessageContent}
-        onSend={handleSend}
-      />
+      {
+        reconnecting
+          ? (
+            <Loading loading text='재접속 중...' />
+          )
+          : (
+            <Loading loading={state !== 4} text={state === 0 ? null : state !== 4 ? '이용자 매칭중입니다!' : null}>
+              <Chat
+                placeholder='메세지를 입력해주세요.'
+                navbar={{ title: '상담' }}
+                messages={messages}
+                renderMessageContent={renderMessageContent}
+                onSend={handleSend}
+              />
+            </Loading>
+          )
+      }
       <style>
         {ChineseCSS}
       </style>
@@ -49,5 +93,3 @@ function Chats () {
 
   )
 }
-
-export default Chats
--- src/routes/ChatBot.jsx
+++ src/routes/ChatBot.jsx
@@ -3,43 +3,66 @@
 import React, { useState, useEffect } from 'react'
 import REST, { baseURL } from '../utils/REST'
 import Logger from '../utils/Logger'
-import Chat, { Bubble, useMessages, toast } from '@chatui/core'
+import Chat, { Flex, FlexItem, Notice, Bubble, useMessages, toast } from '@chatui/core'
+import { Message, Icon } from 'semantic-ui-react'
 // eslint-disable-next-line import/no-webpack-loader-syntax
 import ChineseCSS from '!!raw-loader!@chatui/core/dist/index.css'
 import Loading from '../components/Loading'
 import io from 'socket.io-client'
 
 export default function Chats () {
-  const [isErrored, setErrorState] = useState(true)
+  const [isAwaitMessage, setAwaitMessage] = useState(true)
+  const [isReconnect, setReconnect] = useState(false)
+  const [isWaitForNext, setWaitForNext] = useState(false)
   const [isLoading, setLoading] = useState(true)
   const [socket, setSocket] = useState(false)
-  const { messages, appendMsg, setTyping } = useMessages([])
+  const { messages, appendMsg, setTyping, deleteMsg } = useMessages([])
   localStorage.debug = '*'
   useEffect(() => {
-    const socketConn = io(baseURL + '/chatBot', { transport: ['websocket'] })
+    setReconnect(false)
+    const socketConn = io(baseURL + '/chatBot')
+    socketConn.io.on('reconnect', () => {
+      setReconnect(true)
+    })
     socketConn.on('connect', () => {
+      messages.map(el => deleteMsg(el._id))
+      Logger.info('[Chat] Clear Messages')
       Logger.info(`[Socket.IO] [connect] Socket Connected ${baseURL}`)
       setSocket(socketConn)
       setLoading(false)
+      setAwaitMessage(true)
+      setReconnect(false)
       toast.success('서버와 연결되었습니다 :3')
     })
     socketConn.on('disconnect', () => {
       Logger.warn(`[Socket.IO] [disconnect] Socket Disconnected ${baseURL}`)
     })
+    socketConn.on('returnResult', (data) => {
+      setAwaitMessage(true)
+      appendMsg({
+        type: 'iframe',
+        content: { src: 'https://www.youtube.com/embed/p8vu2j-oVZA' },
+        position: 'left'
+      })
+    })
     socketConn.on('botMessage', (data) => {
       Logger.debug(`[Socket.IO] [botMessage] appendMsg(${data.content})`)
+      setWaitForNext(!data.waitForNext)
       appendMsg({
         type: 'text',
         content: { text: data.content },
         position: 'left'
       })
+      setAwaitMessage(false)
     })
     socketConn.on('messageDenied', (data) => {
       Logger.debug(`[Socket.IO] [messageDenied] messageDenied ${data.content}`)
-      toast.fail('메세지 내용에 비속어가 담겨있어 전송이 취소되었어요 ￣へ￣;')
+      toast.fail('메세지 내용에 비속어가 담겨있어 전송이 취소되었어요.')
+      setAwaitMessage(true)
     })
     socketConn.on('messageAccepted', (data) => {
       Logger.debug(`[Socket.IO] [messageAccepted] appendMsg(${data.content})`)
+      setAwaitMessage(true)
       appendMsg({
         type: 'text',
         content: { text: data.content },
@@ -47,10 +70,14 @@ export default function Chats () {
       })
       setTyping(true)
     })
-  }, [])
+    return isReconnect ? socketConn.destroy() : ''
+  }, [isReconnect])
   function handleSend (type, content) {
     content = content.trim()
     if (content.length <= 0) return
+    if (!(content.length >= 4)) return toast.fail('보낼 메세지의 길이는 4글자 이상이여야 해요.')
+    if (isWaitForNext) return
+    if (isAwaitMessage) return
     Logger.debug(`[Chat] Message Typed ${content} Type: ${type}`)
     if (type !== 'text') return Logger.warn('[Chat] UnSupported Message Type')
     else {
@@ -59,22 +86,41 @@ export default function Chats () {
   }
   function renderMessageContent (msg) {
     const { type, position, content } = msg
-    return <Bubble content={content.text} style={{ backgroundColor: position === 'right' ? '#0084ff' : '#eee' }} />
+    // 빨리 수정점요 ^^아 ㅇㅋ
+    switch (type) {
+      case 'text':
+        return <Bubble content={content.text} style={{ backgroundColor: position === 'right' ? '#0084ff' : '#eee' }} />
+      case 'iframe':
+        return (
+          <Bubble style={{ overflow: 'hidden', width: '100vh', height: '100vh' }}>
+            <iframe src={content.src} style={{ position: 'relative', height: '100%', width: '100%' }} />
+          </Bubble>
+)
+    }
   }
+  /* eslint-disable react/jsx-indent */
   return (
     <>
-      <Loading loading={isLoading}>
-        <Chat
-          navbar={{ title: 'CHAT' }}
-          messages={messages}
-          renderMessageContent={renderMessageContent}
-          onSend={handleSend}
-          placeholder='메세지를 입력해주세요.'
-        />
-        <style>
-          {ChineseCSS}
-        </style>
-      </Loading>
+      {
+        isReconnect
+          ? (
+            <>
+            <Loading loading text='재접속 중...' />
+            </>
+          )
+          : <Loading loading={isLoading}>
+            <Chat
+              navbar={{ title: 'CHAT' }}
+              messages={messages}
+              renderMessageContent={renderMessageContent}
+              onSend={handleSend}
+              placeholder='메세지를 입력해주세요.'
+            />
+            <style>
+              {ChineseCSS}
+            </style>
+            </Loading>
+      }
     </>
   )
 }
