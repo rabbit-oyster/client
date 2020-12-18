deaa2867b5f5c90ced3f6fc53d3b4623bdafb32a
원더 <wonderbotsupport@naver.com>
2020-12-19 04:36:30 +0900

    이스터애그
    Co-Authored-By: Sangoon_Is_Noob <40298015+sannoob@users.noreply.github.com>

--- src/index.jsx
+++ src/index.jsx
@@ -1,9 +1,15 @@
+/* eslint-disable no-useless-escape */
 import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App.jsx'
 import './Root.css'
 import './devices.css'
 import 'semantic-ui-css/semantic.min.css'
+console.log(
+  '%cFOREST OF THE MIND',
+  'color:green; font-size:100px; background:transparent'
+)
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
