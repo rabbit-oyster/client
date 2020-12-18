ecb3fd2b8475bb56de9617a4ac97381162593319
원더 <wonderbotsupport@naver.com>
2020-12-18 21:30:40 +0900

    디바이스 UI 추가

--- src/index.jsx
+++ src/index.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App.jsx'
 import './Root.css'
+import './devices.css'
 import 'semantic-ui-css/semantic.min.css'
 ReactDOM.render(
   <React.StrictMode>
