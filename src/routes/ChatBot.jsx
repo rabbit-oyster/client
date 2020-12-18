2b5f8228f95a2ee62938e2cdeb640143d15009ac
원더 <wonderbotsupport@naver.com>
2020-12-19 00:58:47 +0900

    socketio 구성
    Co-Authored-By: Sangoon_Is_Noob <40298015+sannoob@users.noreply.github.com>

--- src/routes/ChatBot.jsx
+++ src/routes/ChatBot.jsx
@@ -10,12 +10,16 @@ import io from 'socket.io-client'
 
 export default function Chats () {
   const [isErrored, setErrorState] = useState(true)
+  const [isLoading, setLoading] = useState(true)
   const [socket, setSocket] = useState(false)
   const { messages, appendMsg, setTyping } = useMessages([{ type: 'text', content: { text: '안녕하세요! 저는 당신을 사랑하고 있는 산군이에요! 사랑해요~' } }])
+  localStorage.debug = '*'
   useEffect(() => {
-    
+    const socket = io(baseURL, { transport})
+    socket.on('connect', () => {
+    })
+    setLoading(false)
   }, [])
-
   function handleSend (type, content) {
     if (type !== 'text') return
     appendMsg({ type, content: { text: content } })
