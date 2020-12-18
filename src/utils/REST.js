39f9a2d5834a152a523e8a585346ea412bfaa0a6
원더 <wonderbotsupport@naver.com>
2020-12-19 08:16:15 +0900

    병원 센터 데이터 가져오는 함수 추가
    BREAKING CHANGE:
    getToken 함수 필요가 없기에, 삭제
    
    Co-Authored-By: Sangoon_Is_Noob <40298015+sannoob@users.noreply.github.com>

--- src/utils/REST.js
+++ src/utils/REST.js
@@ -1,12 +1,13 @@
 import path from 'path'
 const BASE_URL = 'http://localhost:8000'
+const types = ['전국건강증진센터표준데이터', '정신건강관련전체기관정보']
 class REST {
   constructor () {
     this.serverURL = BASE_URL
   }
 
-  getToken () {
-    return fetch(this.serverURL + '/1')
+  getHospitalData (lat, long, type) { // 0 = 건강증진, 1 = 정신건강 노잼
+    fetch(BASE_URL + '/api/nearest', { method: 'POST', body: JSON.stringify({ Pos: [lat, long], type: types[type] }) })
   }
 }
 export default REST
